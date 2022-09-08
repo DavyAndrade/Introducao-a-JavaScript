163d6c9f1084eeb2011fc9f1c92a13e483e82c47
DavyAndrade <hallszindementahhh@gmail.com>
2022-09-08 08:34:23 -0300

    Introducao a JS

---
+++ PrimeiraQuestão/script.js
@@ -0,0 +1,14 @@
+
+    const lampada = document.getElementById("lamp");
+
+
+    function lampadaAcesa(){
+        lampada.src = "lampOn.jpg"
+    }
+    function lampadaDesligada(){
+        lampada.src = "lampOff.jpg"
+    }
+
+    let container = document.getElementById("container");
+
+      container.style.backgroundColor = '#000000';
---
+++ anotacoes.js
@@ -0,0 +1,36 @@
+/*
+JAVASCRIPT = CLIENTE - SIDE
+
+HTML É ONDE EU COLOCO O MEU CÓDIGO EM JAVASCRIPT
+
+JAVASCRIPT É POPULAR EM MAIS DE 90% DAS PÁGINAS DA INTERNET
+
+CSS É ONDE EU ESTILIZO A MINHA PÁGINA HTML
+
+JAVASCRIPT É ONDE EU DOU FUNCIONALIDADE PARA MINHA PÁGINA
+
+
+    console.log("") - tem a mesma função do "ESCREVA" do Portugol
+
+    -------------------------------
+    
+    DOM = Document Object Model
+    É LÁ QUE EU CONSIGO ESTRUTURAR TODO O MEU DOCUMENTO
+
+    O HTML é o "que". O DOM é "como"
+
+    ----------
+
+    VARIÁVEIS EM JAVASCRIPT
+
+    Existem 3 formas de se declarar variáveis em JS
+
+    var & let = para variáveis que mudam com o tempo
+
+    const = para variáveis que serão sempre constantes
+
+    TIPAGEM DINÂMICA - O Javascript consegue reconhecer automaticamente
+    o tipo de cada variável, sem que eu precise especificar
+    nos minimos detalhes
+
+    */
